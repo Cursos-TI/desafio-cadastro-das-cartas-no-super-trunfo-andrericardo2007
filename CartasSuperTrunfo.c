4495dbce068a526073f39f4bf79347b824722dcd
andrericardo2007 <andrericardobarbosajunior@gmail.com>
2025-03-10 19:40:34 +0000

    EDITADO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,22 +11,23 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
     //Carta 1
-    char(são paulo),
-    char[A01];
-    char[A01], (fortaleza);
-    int pouplaçao = 12325000;
-    float  kilomestros quadrados = 1521.11;
-    float pib = 699.28;
-    int pontos turisticos = 50;
+    char (são paulo),
+    char [A01];
+    char [A01], (fortaleza);
+    int pouplaçao1 = 12325000;
+    float  kilomestros1  = 1521.11;
+    float pib1 = 699.28;
+    int pontos1 = 50;
     
     //Carta 2
     char(rio de janeiro);
-    char[A02];
-    char[A02], (rio de janeiro);
-    int populacao =  6748000;
-    float kilomestros quadrados = 1200.25;
-    float pib = 300.50;
-    int pontos turisticos = 30
+    char [A02];
+    char [A02], (rio de janeiro);
+    int pouplaçao2 = 6748000;
+    float kilomestros2  = 1200.25;
+    float pib2 = 300.50;
+    int pontos2 = 30;
+
 
     
     // Cadastro das Cartas:
