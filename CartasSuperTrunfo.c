0486d14c4f4ada8e87c8d2c7e748e16209466e32
andrericardo2007 <andrericardobarbosajunior@gmail.com>
2025-04-10 19:50:24 +0000

    salvo de novo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,7 +13,7 @@ int main() {
     //Carta 1
     char estado1[5];
     char cidade1[60];
-    int pouplaçao1 = 12325000;
+    int populacao1 = 12325000;
     float  kilomestros1  = 1521.11;
     float pib1 = 699.28;
     int pontos1 = 50;
@@ -24,7 +24,7 @@ int main() {
     //Carta 2
     char estado2[4];
     char cidade2[50];
-    int pouplaçao2 = 6748000;
+    int populacao2 = 6748000;
     float kilomestros2  = 1200.25;
     float pib2 = 300.50;
     int pontos2 = 30;
@@ -32,24 +32,55 @@ int main() {
     float capital2;
 
     //Densidade da carta 1
-    densidade1 = pouplaçao1 / kilomestros1;
+    densidade1 = populacao1 / kilomestros1;
 
      //Densidade da carta 2
-    densidade2 = pouplaçao2 / kilomestros2;
+    densidade2 = populacao2 / kilomestros2;
 
     //PIB per Capita carta 1
-    capital1 = pouplaçao1 / pib1;
+    capital1 = populacao1 / pib1;
 
     
     //PIB per Capita carta 2
-    capital2 = pouplaçao2 / pib2;
+    capital2 = populacao2 / pib2;
 
     printf("Carta 1 \n");
-    printf("estado1 \n");
-  
-
+    printf("Escolha um estado: \n");
+    scanf("%s", &estado1);
+    printf("Escolha uma cidade: \n");
+    scanf("%s", &cidade1);
+    printf("População do local escolido: \n");
+    scanf("%d", &populacao1);
+    printf("Territorio do local escolhido: \n");
+    scanf("%f", &kilomestros1);
+    printf("Pib do local escolhido: \n");
+    scanf("%f", &pib1);
+    printf("Pontos turisticos: \n");
+    scanf("%d", &pontos1);
+    printf("Densidade populacional: \n");
+    scanf("%f", &densidade1);
+    printf("Pib per capta: \n");
+    scanf("%f", &capital1);
    
+    printf("Carta 2 \n");
+    printf("Escolha um estado: \n");
+    scanf("%s", &estado2);
+    printf("Escolha uma cidade: \n");
+    scanf("%s", &cidade2);
+    printf("População do local escolido: \n");
+    scanf("%d", &populacao2);
+    printf("Territorio do local escolhido: \n");
+    scanf("%f", &kilomestros2);
+    printf("Pib do local escolhido:  \n");
+    scanf("%f", &pib2);
+    printf("Pontos turisticos: \n");
+    scanf("%d", &pontos2);
+    printf("Densidade populacional: \n");
+    scanf("%f", &densidade2);
+    printf("Pib per capta: \n");
+    scanf("%f", &capital2);
 
+    
     
     // Cadastro das Cartas: 
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
